39ae97cb75e065057dc57311c115ba1bb66ad0af
HugoStrix <vitorhugomelo5@gmail.com>
2021-06-06 19:50:55 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const init = () => {
+
+    const validateEmail = (event) => {
+        const input = event.currentTarget;
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const emailTest = regex.test(input.value);
+
+        if (!emailTest) {
+            submitButton.setAttribute('disblad', 'disabled');
+            input.nextElementSibling()
+        }
+    }
+
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password"]');
+    const submitButton = document.querySelector('.login__submit');
+
+    if (submitButton) {
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+                })
+            }).then((response) => {
+                return response.json();
+            }).then((data) => {
+                console.log(data)
+            })
+        })
+    }
+}
+
+window.onload = init;
